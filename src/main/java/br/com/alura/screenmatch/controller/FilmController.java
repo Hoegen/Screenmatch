28d3627d2139fb32494b728a1421749c975f938c
Hoegen <marco.caesar@hotmail.com>
2023-06-23 13:54:45 -0300

    Aula 3

--- src/main/java/br/com/alura/screenmatch/controller/FilmController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,24 +21,34 @@ public class FilmController {
 	
 	private List<Film> films = new ArrayList<Film>();
 	
+	
+
+	
 	/**
 	 * @return returns the path for the form
 	 */
 	@GetMapping
+	public String loadListingPage(Model model) {
+		model.addAttribute("list", films);
+		return "films/listing";
+	}
+	
+	/**
+	 * @return returns the path for the form
+	 */
+	@GetMapping("/form")
 	public String loadFormPage() {
 		return "films/form";
 	}
-	
 	
 	/**
 	 * @param data the data that will be posted in the form page
 	 * @return the same page (temporary)
 	 */
-	@PostMapping
+	@PostMapping("/form")
 	public String postFilm(FilmDto data) {
 		films.add(new Film(data));
-		System.out.println(films);
-		return "films/form";
+		return "redirect:/films";
 	}
 	
 }
